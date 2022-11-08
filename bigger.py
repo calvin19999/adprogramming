7eb768c6d5d4d3840406cbb562e4a75672b21f55
calvin19999 <chewcalvin5555@hotmail.com>
2022-11-08 14:25:33 +0800

    First Commit

---
+++ bigger.py
@@ -0,0 +1,6 @@
+first_int=int(input("Insert first number\n"))
+second_int=int(input("insert second number\n"))
+if(first_int>second_int):
+ print("first int is bigger")
+else:
+ print("second int is bigger")
