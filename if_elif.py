6c11fee61b3cb9396a14ffcb8480e348d3e2893b
calvin19999 <chewcalvin5555@hotmail.com>
2022-11-08 15:17:23 +0800

    First Commit

---
+++ if_elif.py
@@ -0,0 +1,11 @@
+percent_float=float(input("what is your percentage? \n"))
+if 90<= percent_float <100:
+ print("you received an A")
+elif 80 <= percent_float <90:
+ print("you received an B")
+elif 70 <= percent_float <80:
+ print("you received an C")
+elif 60 <= percent_float <70:
+ print("you received an D")
+else:
+ print("oops, not good")
